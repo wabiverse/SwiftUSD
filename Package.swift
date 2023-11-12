00425144f35e7db36fbf369aa5bef215211d6146
furby™ <devs@wabi.foundation>
2023-11-12 04:32:51 -0600

    Bump MetaverseKit post openvdb header mishap fix.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
   ],
   dependencies: [
     /* ----------------- a single dependency to rule them all. ----------------- */
-    .package(url: "https://github.com/wabiverse/MetaverseKit.git", from: "1.1.7"),
+    .package(url: "https://github.com/wabiverse/MetaverseKit.git", from: "1.1.8"),
     /* ------------------------------------------------------------------------- */
   ],
   targets: [
