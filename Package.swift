222b63227b7de9a5fa6b64cdb9228c21430472db
furby™ <devs@wabi.foundation>
2023-11-12 18:41:02 -0600

    Fix OpenSubdiv include.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
   ],
   dependencies: [
     /* ----------------- a single dependency to rule them all. ----------------- */
-    .package(url: "https://github.com/wabiverse/MetaverseKit.git", from: "1.1.8"),
+    .package(url: "https://github.com/wabiverse/MetaverseKit.git", from: "1.1.9"),
     /* ------------------------------------------------------------------------- */
   ],
   targets: [
