8ece621a3efda318b79ab1feb5288d30bf29f11b
furby™ <devs@wabi.foundation>
2024-08-05 20:14:03 -0600

    Add back Tf umbrella header.

---
+++ Sources/Tf/include/Tf/Tf.h
@@ -0,0 +1,46 @@
+#ifndef __PXR_BASE_TF_H__
+#define __PXR_BASE_TF_H__
+
+// Tf.
+#include <Tf/CLI11.h>
+#include <Tf/atomicOfstreamWrapper.h>
+#include <Tf/cxxCast.h>
+#include <Tf/declarePtrs.h>
+#include <Tf/diagnosticBase.h>
+#include <Tf/exception.h>
+#include <Tf/expiryNotifier.h>
+#include <Tf/fastCompression.h>
+#include <Tf/getenv.h>
+#include <Tf/hash.h>
+#include <Tf/iterator.h>
+#include <Tf/meta.h>
+#include <Tf/ostreamMethods.h>
+#include <Tf/pyArg.h>
+#include <Tf/pyEnum.h>
+#include <Tf/pyError.h>
+#include <Tf/pyInterpreter.h>
+#include <Tf/pyInvoke.h>
+#include <Tf/pyObjWrapper.h>
+#include <Tf/pyResultConversions.h>
+#include <Tf/pySafePython.h>
+#include <Tf/pyUtils.h>
+#include <Tf/refBase.h>
+#include <Tf/refPtr.h>
+#include <Tf/refPtrTracker.h>
+#include <Tf/robin_growth_policy.h>
+#include <Tf/robin_hash.h>
+#include <Tf/robin_map.h>
+#include <Tf/robin_set.h>
+#include <Tf/scriptModuleLoader.h>
+#include <Tf/setenv.h>
+#include <Tf/smallVector.h>
+#include <Tf/span.h>
+#include <Tf/spinRWMutex.h>
+#include <Tf/staticTokens.h>
+#include <Tf/tf.h>
+#include <Tf/token.h>
+#include <Tf/typeInfoMap.h>
+#include <Tf/unicodeCharacterClasses.h>
+#include <Tf/weakPtr.h>
+
+#endif  // __PXR_BASE_TF_H__
