de120e56459c7f4b9debe3d880d133449704aa97
furby™ <devs@wabi.foundation>
2024-08-05 20:17:44 -0600

    Rename ar.h -> arImpl.h

---
+++ Sources/Ar/include/Ar/arImpl.h
@@ -0,0 +1,13 @@
+//
+// Copyright 2020 Pixar
+//
+// Licensed under the terms set forth in the LICENSE.txt file available at
+// https://openusd.org/license.
+//
+#ifndef PXR_USD_AR_ARIMPL_H
+#define PXR_USD_AR_ARIMPL_H
+
+#define AR_VERSION 2
+
+#endif
+
