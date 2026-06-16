1280ad88a87cbbaf475500647208e8bbaad436ce
Furby™ <furby@wabi.foundation>
2026-06-16 14:42:51 -0600

    Implement apple/SwiftUsd FRTs, ref types, bool operators (Simple/RefBase, UsdStage)
    Signed-off-by: Furby™ <furby@wabi.foundation>

--- Sources/USDOverlays/include/FRTProtocols.h
+++ Sources/USDOverlays/include/FRTProtocols.h
@@ -25,6 +25,25 @@
 #include "Tf/refBase.h"
 #include "SwiftCxxMacros.h"
 
-// stub.
+namespace __Overlay {
+  template <typename T>
+  T* _Nullable dynamic_cast_raw_to_frt(void* _Nullable p) SWIFT_RETURNS_RETAINED {
+    // You can't dynamic_cast from a void* because it has
+    // no RTTI. TfRefBase has RTTI but TfWeakBase doesn't unless
+    // we decide to make its dtor virtual, unlike vanilla OpenUSD.
+    // So for now, let's just assume we'll have a TfRefBase* that
+    // we're trying to cast.
+
+    if constexpr(std::is_base_of_v<Pixar::TfRefBase, T>) {
+      T* result = dynamic_cast<T*>(static_cast<Pixar::TfRefBase*>(p));
+      if (result) {
+        Pixar::Tf_RetainReleaseHelper::retain(result);
+      }
+      return result;
+    } else {
+      static_assert(false, "__Overlay::__dynamic_cast only supports TfRefBase subclasses");
+    }
+  }
+}
 
 #endif /* SWIFTUSD_SWIFTOVERLAY_FRTPROTOCOLS_H */
