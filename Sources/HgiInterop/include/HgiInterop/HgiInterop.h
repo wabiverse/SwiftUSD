ee4af99af9a15fbc12cc78ebaa05a93a9ed61f63
Furby™ <furby@wabi.foundation>
2026-06-18 00:22:32 -0600

    Guard HgiInterop/metal to only macOS in UpdateCommand, and run.
    Signed-off-by: Furby™ <furby@wabi.foundation>

--- Sources/HgiInterop/include/HgiInterop/HgiInterop.h
+++ Sources/HgiInterop/include/HgiInterop/HgiInterop.h
@@ -2,8 +2,10 @@
 #define __PXR_IMAGING_HGIINTEROP_H__
 
 // HgiInterop
-#include <HgiInterop/hgiinteropImpl.h>
+#include <HgiInterop/hgiInteropImpl.h>
 #include <HgiInterop/opengl.h>
 #include <HgiInterop/api.h>
+#if defined(__APPLE__) && defined(__has_include) && __has_include(<AppKit/AppKit.h>)
 #include <HgiInterop/metal.h>
+#endif
 #endif  // __PXR_IMAGING_HGIINTEROP_H__
