0b46ecced1fb6b070f5b54cbc1115a64a5f307c6
Furby™ <furby@wabi.foundation>
2026-06-24 21:04:28 -0600

    Only apply 'mutating' for apple/swiftusd's UsdImagingGL.Engine. * It is an error to specify 'mutating' on Swift class instance methods, it is inferred,   and UsdImagingGL.Engine is a Swift class when annotated with SWIFT_SHARED_REFERENCE.
    Signed-off-by: Furby™ <furby@wabi.foundation>

--- Sources/OpenUSDKit/UsdImaging/UsdImagingGL/Engine.swift
+++ Sources/OpenUSDKit/UsdImaging/UsdImagingGL/Engine.swift
@@ -49,7 +49,66 @@ public extension UsdImagingGL.Engine
       enableUsdDrawModes
     )
   }
+  
+  enum CameraUtilWindowConformPolicy
+  {
+    case matchVertically
+    case matchHorizontally
+    case fit
+    case crop
+    case dontConform
+  }
+  
+  #if canImport(UsdImagingGL)
+  func setEnablePresentation(_ value: Bool)
+  {
+    SetEnablePresentation(value)
+  }
 
+  func setRendererAov(_ aovName: Hd.AovTokens)
+  {
+    SetRendererAov(aovName.token)
+  }
+
+  func render(rootPrim: Usd.Prim, params: UsdImagingGL.RenderParams)
+  {
+    Render(rootPrim, params)
+  }
+
+  func setCameraState(modelViewMatrix: Gf.Matrix4d, projectionMatrix: Gf.Matrix4d)
+  {
+    SetCameraState(modelViewMatrix, projectionMatrix)
+  }
+
+  func setRenderViewport(_ viewport: Gf.Vec4d)
+  {
+    SetRenderViewport(viewport)
+  }
+
+  func setWindowPolicy(_ policy: CameraUtilWindowConformPolicy)
+  {
+    switch policy
+    {
+      case .matchVertically: SetWindowPolicy(Pixar.CameraUtilConformWindowPolicy(0))
+      case .matchHorizontally: SetWindowPolicy(Pixar.CameraUtilConformWindowPolicy(1))
+      case .fit: SetWindowPolicy(Pixar.CameraUtilConformWindowPolicy(2))
+      case .crop: SetWindowPolicy(Pixar.CameraUtilConformWindowPolicy(3))
+      case .dontConform: SetWindowPolicy(Pixar.CameraUtilConformWindowPolicy(4))
+    }
+  }
+
+  func setLightingState(lights: Pixar.GlfSimpleLightVector, material: Pixar.GlfSimpleMaterial, sceneAmbient: Pixar.GfVec4f)
+  {
+    SetLightingState(lights, material, sceneAmbient)
+  }
+  
+  // todo(furbytm): align the return type with apple/swiftusd
+  // by calling the standard 'GetAovTexture' method.
+  func getAovTexture(_ aovName: Hd.AovTokens) -> Pixar.HgiTexture?
+  {
+    GetAovTexturePtr(aovName.token)
+  }
+  #else
   mutating func setEnablePresentation(_ value: Bool)
   {
     SetEnablePresentation(value)
@@ -75,15 +134,6 @@ public extension UsdImagingGL.Engine
     SetRenderViewport(viewport)
   }
 
-  enum CameraUtilWindowConformPolicy
-  {
-    case matchVertically
-    case matchHorizontally
-    case fit
-    case crop
-    case dontConform
-  }
-
   mutating func setWindowPolicy(_ policy: CameraUtilWindowConformPolicy)
   {
     switch policy
@@ -100,23 +150,15 @@ public extension UsdImagingGL.Engine
   {
     SetLightingState(lights, material, sceneAmbient)
   }
-  
-  // todo(furbytm): align the return type with apple/swiftusd
-  // and remove this HydraAovTexture typealias by calling the
-  // 'GetAovTexture' method for both compilation contexts.
-  mutating func getAovTexture(_ aovName: Hd.AovTokens) -> HydraAovTexture
+
+  mutating func getAovTexture(_ aovName: Hd.AovTokens) -> Pixar.HgiTextureHandle
   {
-    #if canImport(UsdImagingGL)
-      GetAovTexturePtr(aovName.token)
-    #else
-      GetAovTexture(aovName.token)
-    #endif
+    GetAovTexture(aovName.token)
   }
+  #endif
 }
 
-#if canImport(OpenUSD)
-public typealias HydraAovTexture = Pixar.HgiTextureHandle
-
+#if !canImport(UsdImagingGL)
 extension Pixar
 {
   public typealias UsdImagingGLEngine = Overlay.UsdImagingGLEngineWrapper
@@ -129,10 +171,4 @@ extension Pixar.UsdImagingGLEngine
   // `UsdImagingGLEngine.Create` to the type itself.
   public typealias Create = UsdImagingGLEngine
 }
-#else
-// todo(furbytm): align the return type for UsdImagingGLEngine's
-// getAovTexture method with apple/swiftusd - we should not have
-// to return an optional HgiTexture. Make it return the standard
-// HgiTextureHandle that the OpenUSD API expects instead.
-public typealias HydraAovTexture = Optional<Pixar.HgiTexture>
 #endif
